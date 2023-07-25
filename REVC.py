7a5fcef3508149a97cd3575a130517ec1ee6eceb
theajs87 <117082737+theajs87@users.noreply.github.com>
2023-07-25 12:20:44 +0700

    Complementing a Strand of DNA

---
+++ REVC.py
@@ -0,0 +1,15 @@
+# Problem 3 - Complementing a Strand of DNA
+revcfile = open('rosalind_revc.txt', 'r')
+s = revcfile.read().rstrip()
+
+scl = list(s)
+for i in range(len(s)):
+    if scl[i] == 'T':
+        scl[i] = 'A'
+    elif scl[i] == 'A':
+        scl[i] = 'T'
+    elif scl[i] == 'G':
+        scl[i] = 'C'
+    elif scl[i] == 'C':
+        scl[i] = 'G'
+''.join(scl)[::-1]
