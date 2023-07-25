c703b6b8cacd23c49ce44b464d4ff62e3b789be4
theajs87 <117082737+theajs87@users.noreply.github.com>
2023-07-25 12:11:43 +0700

    Counting DNA Nucleotides

---
+++ DNA.py
@@ -0,0 +1,20 @@
+# Problem 1 - Counting DNA Nucleotides
+dnafile = open('rosalind_dna.txt', 'r')
+s = dnafile.read().rstrip()
+
+slist = list(s)
+aA = 0
+aC = 0
+aG = 0
+aT = 0
+
+for i in range(len(s)):
+    if slist[i] == 'A':
+        aA += 1
+    if slist[i] == 'C':
+        aC += 1
+    if slist[i] == 'G':
+        aG += 1
+    if slist[i] == 'T':
+        aT += 1
+print(aA, aC, aG, aT)
