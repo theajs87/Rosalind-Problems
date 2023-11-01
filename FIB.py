ce1161f06257aba14721c93355f2029bc806bece
theajs87 <117082737+theajs87@users.noreply.github.com>
2023-11-01 09:01:06 +0700

    231101 removed redundant code

--- FIB.py
+++ FIB.py
@@ -3,7 +3,7 @@ fibfile = open('rosalind_fib.txt', 'r')
 string = fibfile.read().split()
 
 n, k = int(string[0]), int(string[1])
-f1, f2, answer = 1, 1, 0
+f1, f2 = 1, 1
 
 for i in range(n-1):
     f1, f2 = f2, k*f1 + f2
