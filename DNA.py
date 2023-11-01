333080e96d7984695b36fb141e2a5f9b6bc0ef0a
theajs87 <117082737+theajs87@users.noreply.github.com>
2023-11-01 08:54:38 +0700

    231101 cleaned up code

--- DNA.py
+++ DNA.py
@@ -2,17 +2,16 @@
 dnafile = open('rosalind_dna.txt', 'r')
 s = dnafile.read().rstrip()
 
-slist = list(s)
 aA, aC, aG, at = 0, 0, 0, 0
 
-for i in range(len(s)):
-    if slist[i] == 'A':
+for i in s:
+    if i == 'A':
         aA += 1
-    if slist[i] == 'C':
+    if i == 'C':
         aC += 1
-    if slist[i] == 'G':
+    if i == 'G':
         aG += 1
-    if slist[i] == 'T':
+    if i == 'T':
         aT += 1
         
 print(aA, aC, aG, aT)
