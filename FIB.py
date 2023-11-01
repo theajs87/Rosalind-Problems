440ce346eccc0d007547e61f7d69223e0ae52f55
theajs87 <117082737+theajs87@users.noreply.github.com>
2023-11-01 08:59:45 +0700

    231101 cleaned code up

--- FIB.py
+++ FIB.py
@@ -2,10 +2,8 @@
 fibfile = open('rosalind_fib.txt', 'r')
 string = fibfile.read().split()
 
-n = int(string[0])
-k = int(string[1])
-f1 = 1
-f2 = 1
+n, k = int(string[0]), int(string[1])
+f1, f2 = 1, 1
 answer = 0
 for i in range(n-1):
     f1, f2 = f2, k*f1 + f2
